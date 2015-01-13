25929a93bc2cc84bb92c48fddd22e9dad938686d
ipurusho <ipurusho@asu.edu>
2015-01-12 20:52:31 -0500

    forgot to save signalDerivative function

--- signalUtilities.m
+++ signalUtilities.m
@@ -3,4 +3,9 @@ function filtercopy = lowPassFilter(signal,Fs,Fc)
 
 end
 
-function signalDerivative()
+function signalDerivative(filtered_1,filtered_2)
+
+	signalDiff = filtered_1 - filtered_2
+	squaredDiff = signalDiff.^2
+
+end
